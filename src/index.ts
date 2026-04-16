d7c823852aacf9a96fb8415f3647465ae85cabd5
fike110 <chihurumikechukwu05@gmail.com>
2026-04-16 10:49:43 +0100

    hng stage 1 commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+// For Node.js, ensure "type": "module" in package.json
+// (Not required for Bun)
+import { Elysia } from 'elysia';
+ 
+const app = new Elysia().get('/', () => ({
+  message: 'Hello from Elysia on Vercel!',
+}));
+ 
+// Export the Elysia app
+export default app;
